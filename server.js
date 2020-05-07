f6ccc3dd9ede18c6d73a57f7e7ec8ce921f65f68
FitzSilk <60465804+FitzSilk@users.noreply.github.com>
2020-05-07 15:46:25 +0200

    order-test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/eurder-ng'));
+app.use(express.static('./dist/order-test'));
 
 app.listen(process.env.PORT || 8080);
